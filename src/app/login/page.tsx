0902fa7d8db4108b58c69c4d4f9687a40ea986c9
yashdev9274 <yashdev.yvd@gmail.com>
2023-12-18 00:33:34 +0530

    Add login, signup, and profile pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,57 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import {axios} from "axios";
+
+
+
 export default function LoginPage() {
-    return (
-        <div>
-            <h1>Login Page</h1>
+    
+
+    const [user, setUser] = React.useState({
+        email:"",
+        password:"",
+
+    })
+    const onLogin = async() =>{
+    }
+
+    return(
+        <div className="flex flex-col items-center justfiy-center min-h-screen py-2">
+            <h1>Login</h1>
+            <hr/>
+
+            <label htmlFor="email">email</label>
+            <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus: outline-none focus:border-gray-600"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e)=>setUser({...user, email: e.target.value})}
+                placeholder="email"
+            
+            />
+
+            <label htmlFor="password">password</label>
+            <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus: outline-none focus:border-gray-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e)=>setUser({...user, password: e.target.value})}
+                placeholder="password"
+            
+            />
+            
+            <button
+            onClick={onLogin}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus: outline-none focus:border-gray-600"
+            >Login</button>
+
+            <Link href="/signup">Visit Signup Page</Link>
+
         </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -33,7 +33,36 @@ export default function SignupPage() {
                 placeholder="username"
             
             />
+
+            <label htmlFor="email">email</label>
+            <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus: outline-none focus:border-gray-600"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e)=>setUser({...user, email: e.target.value})}
+                placeholder="email"
             
+            />
+
+            <label htmlFor="password">password</label>
+            <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus: outline-none focus:border-gray-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e)=>setUser({...user, password: e.target.value})}
+                placeholder="password"
+            
+            />
+            
+            <button
+            onClick={onSingup}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus: outline-none focus:border-gray-600"
+            >Signup</button>
+
+            <Link href="/login">Visit Login Page</Link>
+
         </div>
     )
 }
